ea62c7af610b2da60b9e2cefbca2ea59ab42ded5
Chris Dessonville <chris@sumologic.com>
2020-03-31 09:26:19 -0700

    SUMO-0 Move log line to debug

--- archaius-aws/src/main/java/com/netflix/config/sources/DynamoDbConfigurationSource.java
+++ archaius-aws/src/main/java/com/netflix/config/sources/DynamoDbConfigurationSource.java
@@ -91,7 +91,7 @@ public class DynamoDbConfigurationSource extends AbstractDynamoDbConfigurationSo
     public PollResult poll(boolean initial, Object checkPoint) throws Exception {
         String table = tableName.get();
         Map<String, Object> map = loadPropertiesFromTable(table);
-        log.info("Successfully polled Dynamo for a new configuration based on table:" + table);
+        log.debug("Successfully polled Dynamo for a new configuration based on table:" + table);
         return PollResult.createFull(map);
     }
 }
